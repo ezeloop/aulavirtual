81c9846d71eb2f369feff934cf4a156aabff6d14
Ezequiel Lopez <56897904+ezeloop@users.noreply.github.com>
2020-04-14 21:21:51 -0300

    Add files via upload

---
+++ Notas-alumnos/index.js
@@ -0,0 +1,11 @@
+alert("Bienvenid@ al aula virtual");
+
+var nota1 = prompt("Ingrese la primer nota: ");
+var nota2 = prompt("Ingrese la segunda nota: ");
+var nota3 = prompt("Ingrese la tercer nota: ");
+var nota4 = prompt("Ingrese la cuarta nota: ");
+var nota5 = prompt("Ingrese la quinta nota: ");
+
+var promedio=(parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3)+parseFloat(nota4)+parseFloat(nota5))/5;
+
+alert("El promedio es: "+promedio);
